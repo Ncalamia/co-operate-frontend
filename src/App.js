1e88de4a17cf0ed40cc84b1c57aa114fc0ba701c
nikki calamia <Ncalamia2468@yahoo.com>
2022-06-30 20:55:36 -0400

    heroku backend is set up and frontend is fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+///////////////////////////////
+//////// States ////////////////
+//////////////////////////////
+
+let [employees, setEmployees] = useState([])
+
+
+
+///////////////////////////////
+//////// CRUD ////////////////
+//////////////////////////////
+
+///////// URLs ///////////////
+const LOCAL_URL = 'http://localhost:8000/api/employees'
+
+const HEROKU_URL = 'https://co-operate-backend.herokuapp.com/api/employees'
+
+//////// READ / FETCH ////////////////
+const getEmployees = () => {
+  axios
+    .get(HEROKU_URL)
+    .then(
+      (response) => setEmployees(response.data),
+      (err) => console.error(err)
+    )
+    .catch((error) => console.error(error))
+ }
+ 
+//////// CREATE //////////////
+
+
+//////// UPDATE //////////////
+
+
+//////// DELETE //////////////
+
+
+
+
+
+
+//////// PAGE LOAD //////////////
+
+ useEffect(() => {
+  getEmployees()
+ }, [])
+ 
+
+
   return (
     <div>
       <h1>Hi</h1>
+      <div className="employees">
+ {employees.map((employee) => {
+   return (
+     <div className="employee" key={employee.id}>
+       <h4>Name: {employee.name}</h4>
+       <h5>Item: {employee.item}</h5>
+       <h5>Image: {employee.image}</h5>
+     </div>
+   )
+ })}
+</div>
     </div>
   );
 }
